6f43a4f13ed9d6f1cb875e1e9f0ff8652521e687
harrymknight <harry.knight@hotmail.co.uk>
2021-07-14 14:48:51 +0100

    Sleep 10 seconds before setting anchor peers

--- test-network/initialise.sh
+++ test-network/initialise.sh
@@ -46,45 +46,47 @@ elif [ "$1" == "up" ]; then
         peer channel join -b ./channel-artifacts/channel1.block
     done
     # Anchor peers cannot be set yet
-#    #Setting the 1st anchor peer
-#    export CORE_PEER_LOCALMSPID="Org1MSP"
-#    export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-#    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-#    export CORE_PEER_ADDRESS=localhost:7051
-#    cd channel-artifacts
-#    configtxlator proto_decode --input config_block.pb --type common.Block --output config_block.json
-#    jq '.data.data[0].payload.data.config' config_block.json > config.json
-#    cp config.json config_copy.json
-#    jq '.channel_group.groups.Application.groups.Org1MSP.values += {"AnchorPeers":{"mod_policy": "Admins","value":{"anchor_peers": [{"host": "peer0.org1.example.com","port": 7051}]},"version": "0"}}' config_copy.json > modified_config.json
-#    configtxlator proto_encode --input config.json --type common.Config --output config.pb
-#    configtxlator proto_encode --input modified_config.json --type common.Config --output modified_config.pb
-#    configtxlator compute_update --channel_id channel1 --original config.pb --updated modified_config.pb --output config_update.pb
-#    configtxlator proto_decode --input config_update.pb --type common.ConfigUpdate --output config_update.json
-#    echo '{"payload":{"header":{"channel_header":{"channel_id":"channel1", "type":2}},"data":{"config_update":'$(cat config_update.json)'}}}' | jq . > config_update_in_envelope.json
-#    configtxlator proto_encode --input config_update_in_envelope.json --type common.Envelope --output config_update_in_envelope.pb
-#    cd ..
-#    peer channel update -f channel-artifacts/config_update_in_envelope.pb -c channel1 -o localhost:7050  --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
-#    #Setting the 2nd anchor peer
-#    export CORE_PEER_LOCALMSPID="Org2MSP"
-#    export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
-#    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
-#    export CORE_PEER_ADDRESS=localhost:9051
-#    peer channel fetch config channel-artifacts/config_block.pb -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com -c channel1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
-#    cd channel-artifacts
-#    configtxlator proto_decode --input config_block.pb --type common.Block --output config_block.json
-#    jq '.data.data[0].payload.data.config' config_block.json > config.json
-#    cp config.json config_copy.json
-#    jq '.channel_group.groups.Application.groups.Org2MSP.values += {"AnchorPeers":{"mod_policy": "Admins","value":{"anchor_peers": [{"host": "peer0.org2.example.com","port": 9051}]},"version": "0"}}' config_copy.json > modified_config.json
-#    configtxlator proto_encode --input config.json --type common.Config --output config.pb
-#    configtxlator proto_encode --input modified_config.json --type common.Config --output modified_config.pb
-#    configtxlator compute_update --channel_id channel1 --original config.pb --updated modified_config.pb --output config_update.pb
-#    configtxlator proto_decode --input config_update.pb --type common.ConfigUpdate --output config_update.json
-#    echo '{"payload":{"header":{"channel_header":{"channel_id":"channel1", "type":2}},"data":{"config_update":'$(cat config_update.json)'}}}' | jq . > config_update_in_envelope.json
-#    configtxlator proto_encode --input config_update_in_envelope.json --type common.Envelope --output config_update_in_envelope.pb
-#    cd ..
-#    peer channel update -f channel-artifacts/config_update_in_envelope.pb -c channel1 -o localhost:7050  --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
-#    #Confirm that the channel has been updated (block height should now be 3)
-#    peer channel getinfo -c channel1
+    #Setting the 1st anchor peer
+    export CORE_PEER_LOCALMSPID="Org1MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:7051
+    sleep 10
+    peer channel fetch config channel-artifacts/config_block.pb -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com -c channel1 --tls --cafile "$ORDERER_CA"
+    cd channel-artifacts
+    configtxlator proto_decode --input config_block.pb --type common.Block --output config_block.json
+    jq '.data.data[0].payload.data.config' config_block.json > config.json
+    cp config.json config_copy.json
+    jq '.channel_group.groups.Application.groups.Org1MSP.values += {"AnchorPeers":{"mod_policy": "Admins","value":{"anchor_peers": [{"host": "peer0.org1.example.com","port": 7051}]},"version": "0"}}' config_copy.json > modified_config.json
+    configtxlator proto_encode --input config.json --type common.Config --output config.pb
+    configtxlator proto_encode --input modified_config.json --type common.Config --output modified_config.pb
+    configtxlator compute_update --channel_id channel1 --original config.pb --updated modified_config.pb --output config_update.pb
+    configtxlator proto_decode --input config_update.pb --type common.ConfigUpdate --output config_update.json
+    echo '{"payload":{"header":{"channel_header":{"channel_id":"channel1", "type":2}},"data":{"config_update":'$(cat config_update.json)'}}}' | jq . > config_update_in_envelope.json
+    configtxlator proto_encode --input config_update_in_envelope.json --type common.Envelope --output config_update_in_envelope.pb
+    cd ..
+    peer channel update -f channel-artifacts/config_update_in_envelope.pb -c channel1 -o localhost:7050  --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
+    #Setting the 2nd anchor peer
+    export CORE_PEER_LOCALMSPID="Org2MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:9051
+    peer channel fetch config channel-artifacts/config_block.pb -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com -c channel1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
+    cd channel-artifacts
+    configtxlator proto_decode --input config_block.pb --type common.Block --output config_block.json
+    jq '.data.data[0].payload.data.config' config_block.json > config.json
+    cp config.json config_copy.json
+    jq '.channel_group.groups.Application.groups.Org2MSP.values += {"AnchorPeers":{"mod_policy": "Admins","value":{"anchor_peers": [{"host": "peer0.org2.example.com","port": 9051}]},"version": "0"}}' config_copy.json > modified_config.json
+    configtxlator proto_encode --input config.json --type common.Config --output config.pb
+    configtxlator proto_encode --input modified_config.json --type common.Config --output modified_config.pb
+    configtxlator compute_update --channel_id channel1 --original config.pb --updated modified_config.pb --output config_update.pb
+    configtxlator proto_decode --input config_update.pb --type common.ConfigUpdate --output config_update.json
+    echo '{"payload":{"header":{"channel_header":{"channel_id":"channel1", "type":2}},"data":{"config_update":'$(cat config_update.json)'}}}' | jq . > config_update_in_envelope.json
+    configtxlator proto_encode --input config_update_in_envelope.json --type common.Envelope --output config_update_in_envelope.pb
+    cd ..
+    peer channel update -f channel-artifacts/config_update_in_envelope.pb -c channel1 -o localhost:7050  --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
+    #Confirm that the channel has been updated (block height should now be 3)
+    peer channel getinfo -c channel1
     #Connecting to 1st orderer as Org1 peer so change environment variables
     export CORE_PEER_LOCALMSPID="Org1MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
